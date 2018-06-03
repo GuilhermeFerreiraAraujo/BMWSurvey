05284c0c3a88f228a809fe257178619c8c25d688
Guilherme Araújo <gui_s3@hotmail.com>
2018-06-03 13:23:11 +0100

    Surveyjs configuration

---
+++ app/src/components/views/Survey/Survey.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+import './Survey.css';
+
+class Survey extends Component {
+  render() {
+    return (
+      <div className="bs-survey">
+       survey component here
+      </div>
+    );
+  }
+}
+
+export default Survey;
